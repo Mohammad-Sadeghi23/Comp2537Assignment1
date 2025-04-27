7649adb616eda9323cdcffb7fbcbf065dda9ac92
Mohammad Sadeghi <mohammad2184@gmail.com>
2025-04-26 22:48:08 -0700

    Added sessions and a page hit counter.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const node_session_secret = '425bfa8d-400a-4394-bdd0-88caa2686f47';
+
+app.use(session({
+    secret: node_session_secret,
+    saveUninitialized: true,
+    resave: false
+}));
+
 // Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the website! This is the home page.');
+
+    if (req.session.pageHits) {
+        pageHits = req.session.pageHits++;
+    } else {
+        pageHits = req.session.pageHits = 1;
+    }
+
+    res.send(`Welcome to the website! ${req.session.pageHits} page hits`);
 });
 
 // Start the server
