cabd185af14b2f2e4e7a666544bc270f595e28cd
Mohammad Sadeghi <mohammad2184@gmail.com>
2025-04-28 10:38:11 -0700

    added sessions and made empty login and sigup pages with links to them in the main page.

--- index.js
+++ index.js
@@ -18,12 +18,40 @@ app.use(session({
 app.get('/', (req, res) => {
 
     if (req.session.pageHits) {
-        pageHits = req.session.pageHits++;
+        req.session.pageHits++;
     } else {
-        pageHits = req.session.pageHits = 1;
+        req.session.pageHits = 1;
     }
 
-    res.send(`Welcome to the website! ${req.session.pageHits} page hits`);
+    var html = `
+    <h1>Welcome to the website!</h1>
+    <p>You have visited ${req.session.pageHits} times.</p>
+    <button><a href="/signup">Signup</a></button>
+    <button><a href="/login">Login</a></button>
+    `;
+    res.send(html);
+});
+
+app.get('/signup', (req, res) => {
+
+    if (req.session.pageHits) {
+        req.session.pageHits++;
+    } else {
+        req.session.pageHits = 1;
+    }
+
+    res.send(`Welcome to the signup! ${req.session.pageHits} page hits`);
+});
+
+app.get('/login', (req, res) => {
+
+    if (req.session.pageHits) {
+        req.session.pageHits++;
+    } else {
+        req.session.pageHits = 1;
+    }
+
+    res.send(`Welcome to the login! ${req.session.pageHits} page hits`);
 });
 
 // Start the server
